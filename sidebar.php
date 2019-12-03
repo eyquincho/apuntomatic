aed41665a6efcc7606431cd390066c3fb4049529
Quincho <joaquin@muruais.com>
2019-12-03 21:53:37 +0100

    Creado archivo independiente

---
+++ sidebar.php
@@ -0,0 +1,79 @@
+    <!------------->
+	<!-- Sidebar -->
+	<!------------->
+    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+      <!-- Logo -->
+      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <div class="sidebar-brand-text mx-3">Apuntomatic</div>
+      </a>
+      <hr class="sidebar-divider my-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Portada</span></a>
+      </li>
+      <hr class="sidebar-divider">
+      <div class="sidebar-heading">
+        Apuntomatic
+      </div>
+		<li class="nav-item">
+        <a class="nav-link" href="apuntes.html">
+          <i class="fas fa-fw fa-folder"></i>
+          <span>Buscar apuntes</span></a>
+		</li>
+		<li class="nav-item">
+        <a class="nav-link" href="subir.html">
+          <i class="fas fa-fw fa-file-upload "></i>
+          <span>Subir apuntes</span></a>
+		</li>
+		<li class="nav-item">
+        <a class="nav-link" href="ranking.html">
+          <i class="fas fa-fw fa-trophy"></i>
+          <span>Ranking</span></a>
+		</li>
+      <hr class="sidebar-divider">
+      <div class="sidebar-heading">
+        Config
+      </div>
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
+          <i class="fas fa-fw fa-user"></i>
+          <span>Perfil</span>
+        </a>
+        <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <a class="collapse-item" href="perfil.html">Mi perfil</a>
+            <a class="collapse-item" href="perfil-edit.html">Editar</a>
+          </div>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="ads.html">
+          <i class="fas fa-fw fa-ad"></i>
+          <span>Publicidad</span></a>
+      </li>
+	  <li class="nav-item">
+        <a class="nav-link" href="admin.html">
+          <i class="fas fa-fw fa-tools"></i>
+          <span>Admin</span></a>
+      </li>
+	  <li class="nav-item">
+        <a class="nav-link" href="privacidad.html">
+          <i class="fas fa-fw fa-user-shield"></i>
+          <span>Privacidad</span></a>
+      </li>
+	  <li class="nav-item">
+        <a class="nav-link" href="cerrar.html">
+          <i class="fas fa-fw fa-sign-out-alt "></i>
+          <span>Cerrar sesión</span></a>
+      </li>
+      <hr class="sidebar-divider d-none d-md-block">
+      <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+      </div>
+
+    </ul>
+    <!----------------->
+	<!-- Fin Sidebar -->
+	<!----------------->
