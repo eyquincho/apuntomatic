1c70ac636999a185118f640b115b554548896fac
Quincho <joaquin@muruais.com>
2020-03-05 21:39:33 +0100

    Arreglado el selector múltiple

---
+++ inc/seleccionar_proceso.php
@@ -0,0 +1,47 @@
+<?php
+// Array que vincula los IDs de los selects declarados en el HTML con el nombre de la tabla donde se encuentra su contenido
+$listadoSelects=array(
+"carreras"=>"ap_carreras",
+"cursos"=>"ap_cursos",
+"asignaturas"=>"ap_asignaturas"
+);
+
+function validaSelect($selectDestino)
+{
+	// Se valida que el select enviado via GET exista
+	global $listadoSelects;
+	if(isset($listadoSelects[$selectDestino])) return true;
+	else return false;
+}
+
+function validaOpcion($opcionSeleccionada)
+{
+	// Se valida que la opcion seleccionada por el usuario en el select tenga un valor numerico
+	if(is_numeric($opcionSeleccionada)) return true;
+	else return false;
+}
+
+$selectDestino=$_GET["select"];
+$opcionSeleccionada=$_GET["opcion"];
+
+if(validaSelect($selectDestino) && validaOpcion($opcionSeleccionada))
+{
+	$tabla=$listadoSelects[$selectDestino];
+	include '../inc/conDB.php';
+	conexionDB();
+	mysqli_query($_SESSION['con'],"SET NAMES 'utf8'");
+	$consulta=mysqli_query($_SESSION['con'], "SELECT id, opcion FROM $tabla WHERE relacion='$opcionSeleccionada'") or die(mysqli_error());
+	
+	// Comienzo a imprimir el select
+	echo "<select class='custom-select col-lg-12' name='".$selectDestino."' id='".$selectDestino."' onChange='cargaContenido(this.id)' class=\"custom-select col-lg-4\">";
+	echo "<option value='0'>Selecciona...</option>";
+	while($registro=mysqli_fetch_row($consulta))
+	{
+		// Convierto los caracteres conflictivos a sus entidades HTML correspondientes para su correcta visualizacion
+		// $registro[1]=urldecode($registro[1]);
+		// Imprimo las opciones del select
+		echo "<option value='".$registro[0]."'>".$registro[1]."</option>";
+	}			
+	echo "</select>";
+}
+?>
