8932a9cb3189d517f252bc66515b94138dcc4a56
Quincho <joaquin@muruais.com>
2020-03-05 21:40:38 +0100

    Organización del archivo

--- apuntes.php
+++ apuntes.php
@@ -4,9 +4,135 @@
 		header("Location: index.php");
 		die();
 	} else {
-include_once("inc/conDB.php");
+require_once("inc/conDB.php");
 conexionDB();
 mysqli_set_charset($_SESSION['con'], 'utf8');
+require("inc/funciones_apuntes.php");
+
+if(isset($_POST['DENboton'])){
+	registrar_denuncia();
+}
+
+if(isset($_FILES['archivo'])){
+	subir_archivo();
+}
+
+if (isset($_POST['asignaturas'])) {
+
+// Recoge las filas de documentos para una asignatura concreta.
+			
+		function mostrar_lista() {
+			$docs = mysqli_query($_SESSION['con'], "SELECT id, usuario_id, asignatura_id, file, size, nombre, descripcion, tipo, descargas, anonimo FROM `ap_documentos` WHERE `asignatura_id` =". $_POST['asignaturas'] ." AND `relacion`=0");
+			while ($seleccionada = mysqli_fetch_object($docs)) {
+				$div_id= $seleccionada->id;
+				$edits = mysqli_query($_SESSION['con'], "SELECT id, usuario_id, asignatura_id, file, size, nombre, descripcion, tipo, descargas, anonimo FROM `ap_documentos` WHERE `original` =". $div_id ." AND `relacion`=1");
+				$num_edits = mysqli_num_rows($edits);
+				$petnomuser = mysqli_query($_SESSION['con'], "SELECT id, user_nick FROM `ap_users` WHERE `ID` = ". $seleccionada->usuario_id . "");
+				$consnomuser = mysqli_fetch_object($petnomuser);
+				if(isset($seleccionada->anonimo) && $seleccionada->anonimo == '1')
+				{
+				$uploader = "Anónimo";
+				}  
+				else {
+				$uploader = $consnomuser->user_nick;
+				}
+				
+				?>
+				<tr>
+				<td><center><i class="fa fa-file-<?php echo $seleccionada->tipo; ?>-o fa-2x"></i></center></td>
+				<td><center><?php echo urldecode($seleccionada->nombre);?></center></td>
+				<td><center><?php echo urldecode($seleccionada->descripcion);?></center></td>
+				<td><center><?php echo number_format($seleccionada->size/1024,2,".",",");?> Mb</center></td>
+				<td><center><a href="<?php echo $seleccionada->file?>" onclick="window.open(\'descargar.php?id=<?php echo $seleccionada->id;?>\')" target="_blank"><i class="fa fa-cloud-download fa-2x"></i></a></center></td>
+				<td><center><?php echo $uploader;?></center></td>
+				<td><center><?php echo $seleccionada->descargas;?></center></td>
+				<td><button title="Denunciar documento" data-target="#modal_denuncia<?php echo $div_id;?>" data-toggle="modal"><i class="fa fa-exclamation-circle text-danger"></i></button></td>
+				<!-- ******************* -->
+				<!-- Modal denunciar documento -->
+				<div class="modal fade" id="modal_denuncia<?php echo $div_id;?>" tabindex="-1" role="dialog" aria-labelledby="ModalDenuncia" aria-hidden="true">
+					<div class="modal-dialog">
+						<div class="modal-content">
+							<div class="modal-header">
+								<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+								<h4 class="modal-title" id="titulo-modal">Denunciar documento </h4> <?php echo urldecode($seleccionada->nombre);?>
+							</div>
+							<div class="modal-body">
+								<form name="enviardenuncia" id="form-denuncia" action="<?php echo $_SERVER['PHP_SELF']; ?>" enctype="multipart/form-data" method="post">
+								<div class="form-group">
+									<label>Motivos</label>
+									<textarea class="form-control" rows="3" id="DENmotivo" name="DENmotivo" placeholder="Describe brevemente los motivos de denuncia"></textarea>
+								</div>
+								<input type="hidden" id="DENarchivo" name="DENarchivo" value="<?php echo $div_id;?>">
+								<input type="hidden" id="DENdenunciante" name="DENdenunciante" value="<?php echo $_SESSION['id']; ?>">
+								<input type="hidden" id="DENacusado" name="DENacusado" value="<?php echo $consnomuser->id; ?>"> 
+							</div>
+							<div class="modal-footer">
+								<input class="btn btn-danger" name="DENboton" type="submit" value="Enviar denuncia" id="DENboton">
+								<a href="#" class="btn" data-dismiss="modal">Cancelar</a>
+							</div></form>
+						</div>
+					</div>
+				</div>
+				<!-- ******************* -->
+				<!-- Tabla extensible de ediciones de documento -->
+				<tbody id="edits<?php echo $div_id;?>" class="collapse hiddenrow accordion-toggle">
+						<?php
+						while ($sel_ed = mysqli_fetch_object($edits)) {
+							$petnomuser_ed = mysqli_query($_SESSION['con'], "SELECT user_nick FROM `ap_users` WHERE `ID` = ". $sel_ed->usuario_id . "");
+							$consnomuser_ed = mysqli_fetch_object($petnomuser_ed);
+							if(isset($sel_ed->anonimo) && $sel_ed->anonimo == '1')
+							{
+							$uploader_ed = "Anónimo";
+							}  
+							else {
+							$uploader_ed = $consnomuser_ed->user_nick;
+							}?>
+								<tr class="success">
+									<td><center><i class="fa fa-file-<?php echo $sel_ed->tipo; ?>-o fa-2x"></i></center></td>
+									<td><center><?php echo urldecode($sel_ed->nombre);?></center></td>
+									<td><center><?php echo urldecode($sel_ed->descripcion);?></center></td>
+									<td><center><?php echo number_format($sel_ed->size/1024,2,".",",");?> Mb</center></td>
+									<td><center><a href="<?php echo $sel_ed->file ?>" onclick="window.open(\'descargar.php?id=<?php echo $sel_ed->id;?>\')" target="_blank"><i class="fa fa-chevron-circle-down fa-2x"></i></a></center></td>
+									<td><center><?php echo $uploader_ed;?></center></td>
+									<td></td>
+									<td><center><?php echo $sel_ed->descargas;?></center></td>
+									<td><button title="Denunciar documento" data-target="#modal_denuncia<?php echo $div_id;?>" data-toggle="modal"><i class="fa fa-exclamation-circle text-danger"></i></button></td>
+								</tr>	
+									<!-- ******************* -->
+									<!-- Modal denunciar ediciones -->
+									<div class="modal fade" id="modal_denuncia<?php echo $sel_ed->id;?>" tabindex="-1" role="dialog" aria-labelledby="ModalDenuncia" aria-hidden="true">
+										<div class="modal-dialog">
+											<div class="modal-content">
+												<div class="modal-header">
+													<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+													<h4 class="modal-title" id="titulo-modal">Denunciar documento </h4> <?php echo urldecode($sel_ed->nombre);?>
+												</div>
+												<div class="modal-body">
+													<form name="enviardenuncia" id="form-denuncia" action="<?php echo $_SERVER['PHP_SELF']; ?>" enctype="multipart/form-data" method="post">
+													<div class="form-group">
+														<label>Motivos</label>
+														<textarea class="form-control" rows="3" id="DENmotivo" name="DENmotivo" placeholder="Describe brevemente los motivos de denuncia"></textarea>
+													</div>
+													<input type="hidden" id="DENarchivo" name="DENarchivo" value="<?php echo $sel_ed->id;?>">
+													<input type="hidden" id="DENdenunciante" name="DENdenunciante" value="<?php echo $_SESSION['id']; ?>">
+													<input type="hidden" id="DENacusado" name="DENacusado" value="<?php echo $consnomuser_ed->user_nick; ?>"> 
+												</div>
+												<div class="modal-footer">
+													<input class="btn btn-danger" name="DENboton" type="submit" value="Enviar denuncia" id="DENboton">
+													<a href="#" class="btn" data-dismiss="modal">Cancelar</a>
+												</div></form>
+											</div>
+										</div>
+									</div>
+									<!-- ******************* -->								
+							<?php
+						}
+						?>
+				</tbody>
+				<?php			
+			}
+		}
+	}
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -29,7 +155,18 @@ mysqli_set_charset($_SESSION['con'], 'utf8');
 </head>
 
 <body id="page-top">
-
+  <?php 
+		$perfil = mysqli_query($_SESSION['con'], "SELECT * FROM ap_users WHERE user_nick='".$_SESSION['nick']."'") or die(mysql_error());
+		$row = mysqli_fetch_array($perfil);
+		$id_sesion = $row["ID"];
+		$nick_sesion = $row["user_nick"];
+		$nom_sesion = $row["user_name"];
+		$freg_sesion = $row["user_registered"];
+		$sub_sesion = $row["user_files"];
+		$desc_sesion = $row["user_downloads"];
+		//gravatar
+		$gravatarMd5 = md5($row["user_email"]);
+		?>
   <!-- Page Wrapper -->
   <div id="wrapper">
 
@@ -61,36 +198,32 @@ mysqli_set_charset($_SESSION['con'], 'utf8');
                 </div>
                 <!-- Card Body -->
                 <div class="card-body">
-				  <form class="form-inline">
-					<label class="sr-only" for="Select1">Universidad</label>
-					<select class="custom-select col-lg-3" id="Select1">
-						<option selected>Universidad</option>
-						<option value="1">One</option>
-						<option value="2">Two</option>
-						<option value="3">Three</option>
-					</select>
-					<label class="sr-only" for="Select2">Carrera</label>
-					<select class="custom-select col-lg-3" id="Select2">
-						<option selected>Titulación</option>
-						<option value="1">One</option>
-						<option value="2">Two</option>
-						<option value="3">Three</option>
-					</select>
-					<label class="sr-only" for="Select3">Curso</label>
-					<select class="custom-select col-lg-3" id="Select3">
-						<option selected>Curso</option>
-						<option value="1">One</option>
-						<option value="2">Two</option>
-						<option value="3">Three</option>
-					</select>
-					<label class="sr-only" for="Select4">Asignatura</label>
-					<select class="custom-select col-lg-3" id="Select4">
-						<option selected>Asignatura</option>
-						<option value="1">One</option>
-						<option value="2">Two</option>
-						<option value="3">Three</option>
-					</select>
-				  </form>
+                  <!-- Selector asignaturas -->
+                    <form class="form-inline" action="<?php echo $_SERVER['PHP_SELF']; ?>" enctype="multipart/form-data" method="post" name="form">
+					<div class="form-group col-md-4">
+					  <label class="sr-only" for="carreras">Titulación</label>
+                        <select class="custom-select col-sm-12" name="carreras" id="carreras" onChange="cargaContenido(this.id)" >
+                          <option value='0' >Selecciona Titulación</option>
+                          <?php generaCarreras(); ?>
+                        </select>
+					</div>
+					<div class="form-group col-md-4">
+                      <label class="sr-only" for="cursos">Curso</label>
+                        <select class="custom-select col-sm-12" name="cursos" id="cursos" disabled>
+                          <option value="0">Selecciona Curso</option>
+                        </select>
+					</div>
+					<div class="form-group col-md-4">
+                      <label class="sr-only" for="asignaturas">Asignatura</label>
+                        <select class="custom-select col-sm-12" name="asignaturas" id="asignaturas" disabled>
+                          <option value="0">Selecciona Asignatura</option>
+                        </select>
+					</div>
+					<div class="form-group col-md-4">
+					<button type="submit" name="buscar" class="btn btn-primary">Buscar</button>
+					</div>
+                    </form>
+                  <!-- Selector asignaturas FIN -->
                 </div>
               </div>
             </div>
@@ -105,7 +238,7 @@ mysqli_set_charset($_SESSION['con'], 'utf8');
               <div class="card shadow mb-4">
                 <!-- Card Header - Dropdown -->
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                  <h6 class="m-0 font-weight-bold text-primary">Documentos disponibles de ###</h6>
+                  <h6 class="m-0 font-weight-bold text-primary"><?php mostrar_asignatura(); ?></h6>
                 </div>
                 <!-- Card Body -->
                 <div class="card-body">
@@ -113,21 +246,21 @@ mysqli_set_charset($_SESSION['con'], 'utf8');
                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                   <thead>
                     <tr>
-                      <th>Doc</th>
-                      <th>Usuario</th>
-                      <th>Documento</th>
-                      <th>Asignatura</th>
-                      <th>Titulación</th>
+                      <th>Formato</th>
+                      <th>Nombre</th>
+                      <th>Descripción</th>
+                      <th>Fecha</th>
+                      <th>Descargas</th>
                       <th>Enlace</th>
                     </tr>
                   </thead>
                   <tfoot>
                     <tr>
-                      <th>Doc</th>
-                      <th>Usuario</th>
-                      <th>Documento</th>
-                      <th>Asignatura</th>
-                      <th>Titulación</th>
+					  <th>Formato</th>
+                      <th>Nombre</th>
+                      <th>Descripción</th>
+                      <th>Fecha</th>
+                      <th>Descargas</th>
                       <th>Enlace</th>
                     </tr>
                   </tfoot>
@@ -244,10 +377,12 @@ mysqli_set_charset($_SESSION['con'], 'utf8');
   <script src="js/sb-admin-2.js"></script>
   <script src="js/jquery.dataTables.min.js"></script>
   <script src="js/dataTables.bootstrap4.min.js"></script>
+  <script src="inc/seleccionar.js"></script>
 
   <!-- JavaScript propio -->
   <script src="js/tablas-apuntomatic.js"></script>
 
 </body>
+
 </html>
 <?php } ?>   
---
+++ inc/funciones_apuntes.php
@@ -0,0 +1,93 @@
+<?php
+
+// Si se denuncia un archivo, guardamos la denuncia en la base de datos
+function registrar_denuncia() {
+    $DENarchivo = $_POST['DENarchivo'];
+    $DENacusado = $_POST['DENacusado'];
+    $DENdenunciante = $_POST['DENdenunciante'];
+    $DENmotivo = $_POST['DENmotivo'];
+       if(!empty($DENmotivo)) { 
+        $qry_denuncia = "INSERT INTO ap_denuncias ( IDarchivo, IDacusado, IDdenunciante, motivo, resuelto ) VALUES 
+        ('$DENarchivo', '$DENacusado','$DENdenunciante','$DENmotivo','0')";
+        mysqli_query($_SESSION['con'], $qry_denuncia);
+        echo "<div style=\"margin-top:40px;\" class=\"alert alert-success\" role=\"alert\">Denuncia enviada. Gracias!</div>";  }
+        else {
+            echo "<div style=\"margin-top:40px;\" class=\"alert alert-danger\" role=\"alert\">No escribiste un motivo de denuncia</div>";
+        }
+}
+
+
+// Lanza el select de asignaturas
+function generaCarreras()
+{
+    $consulta = mysqli_query($_SESSION['con'], "SELECT `id`, `opcion` FROM `ap_carreras`");
+    // Voy imprimiendo el primer select compuesto por los carreras
+    while($registro=mysqli_fetch_row($consulta))
+    {
+        echo "<option value='".$registro[0]."'>".$registro[1]."</option>";
+    }
+}
+
+function subir_archivo()
+{
+    $uploaddir = "documentos/";
+		$file = time() . '-' . $_FILES['archivo']['name'];
+		$file = preg_replace('/[^0-9a-zA-Z-_.]+/','',$file);
+ 		$uploadfile = $uploaddir . $file;
+ 		$error = $_FILES['archivo']['error'];
+ 		$subido = false;
+ 		$tipoch = str_replace("application/","",$_FILES["archivo"]["type"]);
+		switch ($tipoch) {
+		    case "vnd.openxmlformats-officedocument.wordprocessingml.document":
+		        $tipo = "docx";
+		        break;
+		    case "vnd.ms-excel":
+		        $tipo = "xls";
+		        break;
+		    case "vnd.openxmlformats-officedocument.spreadsheetml.sheet":
+		        $tipo = "xlsx";
+		        break;
+		    case "vnd.ms-powerpoint":
+		        $tipo = "ppt";
+		        break;
+		    case "ax-rar-compressed":
+		        $tipo = "archivo";
+		        break;
+		}
+		$n = $_SESSION["nick"];
+		$query = "SELECT `ID` FROM `ap_users` WHERE `user_nick`='$n'";
+		$query2 = mysqli_query($_SESSION['con'], $query);
+		$fila = mysqli_fetch_array($query2);
+		$user_id = $fila["ID"];
+		$descripcion = urlencode($_POST['descripcion']);
+		$size = $_FILES["archivo"]["size"] / 1024;
+		$original = $_POST['original'];
+		if(isset($_POST['anon']))
+			$anonimo = true;
+		else
+			$anonimo = false;
+		if(isset($_POST['boton']) && $error==UPLOAD_ERR_OK) { 
+		   $subido = copy($_FILES['archivo']['tmp_name'], $uploadfile); 
+		   $check = $subido && !empty($descripcion);
+		}
+	   	if($check) { 
+			$qry_edit = "INSERT INTO ap_documentos ( usuario_id, creado_ts, file, size, descripcion, tipo, anonimo, relacion, original ) VALUES 
+			('$user_id', CURDATE(), '$uploadfile','$size','$descripcion','$tipo','$anonimo','1','$original')";
+			mysqli_query($_SESSION['con'], $qry_edit);
+			$update_user = "UPDATE ap_users SET user_edits=(user_edits + 1) WHERE id='$user_id'";
+			mysqli_query($_SESSION['con'], $update_user);
+			echo "<div style=\"margin-top:40px;\" class=\"alert alert-success\" role=\"alert\">Archivo subido correctamente</div>";  }
+}
+
+
+// Muestra el nombre de la asignatura
+
+function mostrar_asignatura(){
+    if (isset($_POST['asignaturas'])){
+    $pet_asign = mysqli_query($_SESSION['con'], "SELECT opcion FROM `ap_asignaturas` WHERE `id` =". $_POST['asignaturas'] ."");
+    $asign_elegida = mysqli_fetch_row($pet_asign);
+    echo 'Documentos disponibles de '. $asign_elegida[0] . '.';
+}else{}
+}
+
+?>
