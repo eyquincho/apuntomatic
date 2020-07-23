073a9508a06dcc97a5c1b4ba541d59543a3b752b
eyquincho <joaquin@muruais.com>
2020-07-23 13:09:43 +0200

    Creado

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer class="sticky-footer bg-white">
+	<div class="container my-auto">
+		<div class="copyright text-center my-auto">
+			<span>Apuntomatic - <a href="https://github.com/eyquincho/apuntomatic" target="_blank">GitHub</span> 
+		</div>
+	</div>
+</footer>
