b3d1690c581d97df0936b6e8238c7776a8656462
eyquincho <joaquin@muruais.com>
2020-08-20 14:17:22 +0200

    Funcional

--- registro.php
+++ registro.php
@@ -9,6 +9,46 @@ if(isset($_SESSION['nick'])){
   header("Location: main.php");
   die();
 } else {}
+
+function guardar_registro(){
+        // Procedemos a comprobar que los campos del formulario no estén vacíos
+        $sin_espacios = count_chars($_POST['usuario_nombre'], 1);
+        if(!empty($sin_espacios[32])) { // comprobamos que el campo usuario_nombre no tenga espacios en blanco
+            echo "<div class=\"alert alert-warning\" role=\"alert\">El nick no puede contener espacios en blanco. <a href='javascript:history.back();'>Reintentar</a></div>";
+        }elseif(empty($_POST['usuario_nombre'])) { // comprobamos que el campo usuario_nombre no esté vacío
+            echo "<div class=\"alert alert-warning\" role=\"alert\">No has ingresado un nombre de usuario. <a href='javascript:history.back();'>Reintentar</a></div>";
+        }elseif(empty($_POST['usuario_clave'])) { // comprobamos que el campo usuario_clave no esté vacío
+            echo "<div class=\"alert alert-warning\" role=\"alert\">No has puesto ninguna contraseña. <a href='javascript:history.back();'>Reintentar</a></div>";
+        }elseif($_POST['usuario_clave'] != $_POST['usuario_clave_conf']) { // comprobamos que las contraseñas ingresadas coincidan
+            echo "<div class=\"alert alert-warning\" role=\"alert\">Las contraseñas ingresadas no coinciden. <a href='javascript:history.back();'>Reintentar</a></div>";
+        }elseif(!filter_var($_POST['usuario_email'], FILTER_VALIDATE_EMAIL)) { // validamos que el email ingresado sea correcto
+            echo "<div class=\"alert alert-warning\" role=\"alert\">No has insertado una dirección de email válida. <a href='javascript:history.back();'>Reintentar</a></div>";
+        }else {
+            // "limpiamos" los campos del formulario de posibles códigos maliciosos
+            $usuario_nombre = mysqli_real_escape_string($_SESSION['con'], $_POST['usuario_nombre']);
+            $usuario_clave = mysqli_real_escape_string($_SESSION['con'], $_POST['usuario_clave']);
+            $usuario_email = mysqli_real_escape_string($_SESSION['con'], $_POST['usuario_email']);
+            // comprobamos que el usuario ingresado no haya sido registrado antes
+            $sql_nick = mysqli_query($_SESSION['con'], "SELECT user_nick FROM ap_users WHERE user_nick='".$usuario_nombre."'");
+			$sql_mail = mysqli_query($_SESSION['con'], "SELECT user_email FROM ap_users WHERE user_email='".$usuario_email."'");
+            if(mysqli_num_rows($sql_nick) > 0) {
+                echo "<div class=\"alert alert-danger\" role=\"alert\">El nombre usuario elegido ya ha sido registrado anteriormente. <a href='javascript:history.back();'>Reintentar</a></div>";
+            }else {
+				if(mysqli_num_rows($sql_mail) > 0) {
+                echo "<div class=\"alert alert-danger\" role=\"alert\">El email que has utilizado ya está siendo utilizado. <br />¿Es tu cuenta? ¡Recupera tu contraseña! <a href='javascript:history.back();'>Reintentar</a></div>";
+				}else {
+					$usuario_clave = md5($usuario_clave); // encriptamos la contraseña ingresada con md5
+					// ingresamos los datos a la BD
+					$reg = mysqli_query($_SESSION['con'],"INSERT INTO ap_users (user_nick, user_pass, user_email, user_registered) VALUES ('".$usuario_nombre."', '".$usuario_clave."', '".$usuario_email."', NOW())");
+					if($reg) {
+						echo "<div class=\"alert alert-success\" role=\"alert\">Registro realizado con éxito, ahora ya puedes iniciar sesión y empezar a compartir.</div>";
+					}else {
+						echo "<div class=\"alert alert-warning\" role=\"alert\">Ha ocurrido un error, por favor, ponte en contacto con el Administrador.</div>";
+					}
+				}
+			}
+        }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -52,25 +92,30 @@ include "modals/galletas.html";
 					<hr>
 				<h1 class="h4 text-gray-900 mb-4">Crear una cuenta nueva</h1>
               </div>
-              <form class="user">
+							<?php
+								if(isset($_POST['enviar'])) { 	
+									guardar_registro();
+								}else {}
+							?>
+              <form class="user" id="regForm" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" >
                 <div class="form-group">
-                    <input type="text" class="form-control form-control-user" id="exampleFirstName" placeholder="Usuario">
+                    <input type="text" class="form-control form-control-user" name="usuario_nombre" id="exampleFirstName" placeholder="Usuario" required>
                 </div>
                 <div class="form-group">
-                  <input type="email" class="form-control form-control-user" id="exampleInputEmail" placeholder="Correo electrónico">
+                  <input type="email" class="form-control form-control-user" name="usuario_email" id="exampleInputEmail" placeholder="Correo electrónico" required>
                 </div>
                 <div class="form-group row">
                   <div class="col-sm-6 mb-3 mb-sm-0">
-                    <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Contraseña">
+                    <input type="password" class="form-control form-control-user" name="usuario_clave" id="exampleInputPassword" placeholder="Contraseña" required>
                   </div>
                   <div class="col-sm-6">
-                    <input type="password" class="form-control form-control-user" id="exampleRepeatPassword" placeholder="Repetir contraseña">
+                    <input type="password" class="form-control form-control-user" name="usuario_clave_conf" id="exampleRepeatPassword" placeholder="Repetir contraseña" required>
                   </div>
                 </div>
 				<p class="mb-4">Al registrarte aceptas nuestras <a data-toggle="modal" data-target="#condiciones" href="#">Condiciones de uso</a></p>
-                <a href="login.html" class="btn btn-primary btn-user btn-block">
-                  Regístrate
-                </a>
+                <button type="submit" name="enviar" id="enviar" class="btn btn-primary btn-user btn-block">
+                  Enviar
+                </button>
               </form>
               <hr>
               <div class="text-center">
