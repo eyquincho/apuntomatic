469df58d231138c061e64ebfe13e09fd8d2f99aa
eyquincho <joaquin@muruais.com>
2020-08-20 14:17:05 +0200

    Terminado

--- perfil.php
+++ perfil.php
@@ -7,6 +7,40 @@
 include_once("inc/conDB.php");
 conexionDB();
 mysqli_set_charset($_SESSION['con'], 'utf8');
+
+$tbl_name ="ap_users";
+$id_usuario = $_GET['id'];
+$sql="SELECT * FROM $tbl_name WHERE ID='$id_usuario'";
+$result=mysqli_query($_SESSION['con'], $sql);
+$user_sql = mysqli_fetch_object($result);
+$count=mysqli_num_rows($result);
+if($count==0){
+	//modificar para renvíe al perfil del usuario activo con aviso de usuario no encontrado
+	header("Location: perfil.php?id=".$_SESSION['id']."&ec=1");
+}else{}
+	$md5email = trim ($user_sql->user_email); // "MyEmailAddress@example.com"
+	$md5email = strtolower( $md5email ); // "myemailaddress@example.com"
+	$emailhash= md5( $md5email );
+function mostrar_lista() {
+	$docs_tabla = mysqli_query($_SESSION['con'], "SELECT id, creado_ts, usuario_id, asignatura_id, file, size, nombre, descripcion, tipo, descargas, anonimo FROM `ap_documentos` WHERE `relacion`=0 AND `usuario_id`=".$_GET['id']." AND `anonimo`=0 AND NOT (`asignatura_id`= 4 OR `asignatura_id`= 38) ORDER BY `id` DESC");
+	while ($seleccionada = mysqli_fetch_object($docs_tabla)) {
+		$petnomuser = mysqli_query($_SESSION['con'], "SELECT user_nick FROM `ap_users` WHERE `ID` = ". $seleccionada->usuario_id . "");
+		$consnomuser = mysqli_fetch_object($petnomuser);
+		$sql_asignatura = mysqli_query($_SESSION['con'], "SELECT opcion, relacion FROM `ap_asignaturas` WHERE `ID` = ". $seleccionada->asignatura_id . "");
+		$pet_asignatura = mysqli_fetch_object($sql_asignatura);
+		$sql_curso = mysqli_query($_SESSION['con'], "SELECT opcion, relacion FROM `ap_cursos` WHERE `id` = ". $pet_asignatura->relacion . "");
+		$pet_curso = mysqli_fetch_object($sql_curso);
+		$sql_titulacion = mysqli_query($_SESSION['con'], "SELECT opcion FROM `ap_carreras` WHERE `id` = ". $pet_curso->relacion . "");
+		$pet_titulacion = mysqli_fetch_object($sql_titulacion);
+    echo '<tr>';
+    echo '<td><center>' . urldecode($seleccionada->nombre) . '</center></td>';
+    echo '<td><center>' . $pet_asignatura->opcion . '</center></td>';
+		echo '<td><center>' . $pet_titulacion->opcion . '</center></td>';
+		echo '<td><center>' . date("d-m-Y", strtotime($seleccionada->creado_ts)) . '</center></td>';
+		echo '<td><center><a href="'. $seleccionada->file .'" onclick="window.open(\'descargar.php?id='. $seleccionada->id .'\')" target="_blank"><i class="fa fa-chevron-circle-down fa-2x"></i></a></center></td>';
+		echo '</tr>';  
+  }
+}
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -35,60 +69,47 @@ mysqli_set_charset($_SESSION['con'], 'utf8');
       <div id="content">
           <?php include "header.php" ?>
         <div class="container-fluid">
+					<?php
+						if (isset($_GET['ec']) && $_GET['ec']==1){
+							echo "<div class=\"alert alert-warning\" role=\"alert\">El usuario que buscas no existe</div>";
+						}else{}
+					?>
           <!-- Content Row -->
 
           <div class="row">
 						
             <!-- Pie Chart -->
-            <div class="col-xl-4 col-lg-5">
+            <div class="col-lg-4 col-md-12">
               <div class="card shadow mb-4">
                 <!-- Card Header - Dropdown -->
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                  <h6 class="m-0 font-weight-bold text-primary">Perfil de <?php echo $_SESSION['nick']; ?></h6>
+                  <h6 class="m-0 font-weight-bold text-primary">Perfil de <?php echo $user_sql->user_nick; ?></h6>
                 </div>
                 <!-- Card Body -->
                 <div class="card-body">
 									<div class="row">
-									<div class="col-sm-6 col-md-4">
-                        <img src="http://placehold.it/380x500" alt="" class="img-rounded img-responsive" />
-                    </div>
-                    <div class="col-sm-6 col-md-8">
-                        <h4>
-                            Bhaumik Patel</h4>
-                        <small><cite title="San Francisco, USA">San Francisco, USA <i class="glyphicon glyphicon-map-marker">
-                        </i></cite></small>
+									<div class="col-4">
+                    <img class="img-profile rounded-circle" src="https://www.gravatar.com/avatar/<?php echo $emailhash; ?>?s=150" style="width:100%">
+                  </div>
+                  <div class="col-8">
+                        <h4><?php echo $user_sql->user_nick; ?></h4>
+                        <small>Miembro desde <?php echo date("d-m-Y", strtotime($user_sql->user_registered));?></small>
                         <p>
-                            <i class="glyphicon glyphicon-envelope"></i>email@example.com
-                            <br />
-                            <i class="glyphicon glyphicon-globe"></i><a href="http://www.jquery2dotnet.com">www.jquery2dotnet.com</a>
-                            <br />
-                            <i class="glyphicon glyphicon-gift"></i>June 02, 1988</p>
-                        <!-- Split button -->
-                        <div class="btn-group">
-                            <button type="button" class="btn btn-primary">
-                                Social</button>
-                            <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
-                                <span class="caret"></span><span class="sr-only">Social</span>
-                            </button>
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="#">Twitter</a></li>
-                                <li><a href="https://plus.google.com/+Jquery2dotnet/posts">Google +</a></li>
-                                <li><a href="https://www.facebook.com/jquery2dotnet">Facebook</a></li>
-                                <li class="divider"></li>
-                                <li><a href="#">Github</a></li>
-                            </ul>
-                        </div>
+                          <a href="#" target="_blank"><i class="fas fa-globe fa-2x"></i></a>
+													<a href="#" target="_blank"><i class="fab fa-twitter-square fa-2x"></i></a>
+												</p>
                     </div>
 									
                 </div>
               </div>
             </div>
+						</div>
             <!-- Area Chart -->
-            <div class="col-xl-8 col-lg-7">
+            <div class="col-lg-8 col-md-12">
               <div class="card shadow mb-4">
                 <!-- Card Header - Dropdown -->
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                  <h6 class="m-0 font-weight-bold text-primary">Documentos de <?php echo $_SESSION['nick']; ?></h6>
+                  <h6 class="m-0 font-weight-bold text-primary">Documentos de <?php echo $user_sql->user_nick; ?></h6>
                 </div>
                 <!-- Card Body -->
                 <div class="card-body">
@@ -96,81 +117,24 @@ mysqli_set_charset($_SESSION['con'], 'utf8');
                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                   <thead>
                     <tr>
-                      <th>Doc</th>
-                      <th>Usuario</th>
                       <th>Documento</th>
                       <th>Asignatura</th>
                       <th>Titulación</th>
+					            <th>Fecha</th>
                       <th>Enlace</th>
                     </tr>
                   </thead>
                   <tfoot>
                     <tr>
-                      <th>Doc</th>
-                      <th>Usuario</th>
                       <th>Documento</th>
                       <th>Asignatura</th>
                       <th>Titulación</th>
+					            <th>Fecha</th>
                       <th>Enlace</th>
                     </tr>
                   </tfoot>
                   <tbody>
-                    <tr>
-                      <td>Tiger Nixon</td>
-                      <td>System Architect</td>
-                      <td>Edinburgh</td>
-                      <td>61</td>
-                      <td>2011/04/25</td>
-                      <td><center><i class="fas fa-file-download"></i> 14 Mb</center></td>
-                    </tr>
-                    <tr>
-                      <td>Garrett Winters</td>
-                      <td>Accountant</td>
-                      <td>Tokyo</td>
-                      <td>63</td>
-                      <td>2011/07/25</td>
-                      <td><center><i class="fas fa-file-download"></i> 14 Mb</center></td>
-                    </tr>
-                    <tr>
-                      <td>Ashton Cox</td>
-                      <td>Junior Technical Author</td>
-                      <td>San Francisco</td>
-                      <td>66</td>
-                      <td>2009/01/12</td>
-                      <td><center><i class="fas fa-file-download"></i> 14 Mb</center></td>
-                    </tr>
-                    <tr>
-                      <td>Cedric Kelly</td>
-                      <td>Senior Javascript Developer</td>
-                      <td>Edinburgh</td>
-                      <td>22</td>
-                      <td>2012/03/29</td>
-                      <td><center><i class="fas fa-file-download"></i> 14 Mb</center></td>
-                    </tr>
-                    <tr>
-                      <td>Airi Satou</td>
-                      <td>Accountant</td>
-                      <td>Tokyo</td>
-                      <td>33</td>
-                      <td>2008/11/28</td>
-                      <td><center><i class="fas fa-file-download"></i> 14 Mb</center></td>
-                    </tr>
-                    <tr>
-                      <td>Brielle Williamson</td>
-                      <td>Integration Specialist</td>
-                      <td>New York</td>
-                      <td>61</td>
-                      <td>2012/12/02</td>
-                      <td><center><i class="fas fa-file-download"></i> 14 Mb</center></td>
-                    </tr>
-                    <tr>
-                      <td>Herrod Chandler</td>
-                      <td>Sales Assistant</td>
-                      <td>San Francisco</td>
-                      <td>59</td>
-                      <td>2012/08/06</td>
-                      <td><center><i class="fas fa-file-download"></i> 14 Mb</center></td>
-                    </tr>
+                    <?php mostrar_lista(); ?>
                   </tbody>
                 </table>
               </div>
