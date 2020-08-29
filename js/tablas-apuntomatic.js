a752dcb3d85aba025365134505f4d44d914c964d
Quincho <joaquin@muruais.com>
2020-08-29 16:10:04 +0200

    Nuevo tipo de tabla perfil

--- js/tablas-apuntomatic.js
+++ js/tablas-apuntomatic.js
@@ -31,31 +31,60 @@ $(document).ready(function() {
   }
   });
   });
-  $(document).ready(function() {
-    $('#dataTable').DataTable( {
-    "language": {
-      "sProcessing":     "Procesando...",
-      "sLengthMenu":     "Mostrar _MENU_ documentos",
-      "sZeroRecords":    "No se encontraron resultados",
-      "sEmptyTable":     "Ningún dato disponible en esta tabla",
-      "sInfo":           "Mostrando documentos del _START_ al _END_ de un total de _TOTAL_ documentos",
-      "sInfoEmpty":      "Mostrando documentos del 0 al 0 de un total de 0 documentos",
-      "sInfoFiltered":   "(filtrado de un total de _MAX_ documentos)",
-      "sInfoPostFix":    "",
-      "sSearch":         "Buscar:",
-      "sUrl":            "",
-      "sInfoThousands":  ",",
-      "sLoadingRecords": "Cargando...",
-      "oPaginate": {
-          "sFirst":    "Primero",
-          "sLast":     "Último",
-          "sNext":     "Siguiente",
-          "sPrevious": "Anterior"
-      },
-      "oAria": {
-          "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
-          "sSortDescending": ": Activar para ordenar la columna de manera descendente"
-      }
+$(document).ready(function() {
+  $('#dataTable').DataTable( {
+  "language": {
+    "sProcessing":     "Procesando...",
+    "sLengthMenu":     "Mostrar _MENU_ documentos",
+    "sZeroRecords":    "No se encontraron resultados",
+    "sEmptyTable":     "Ningún dato disponible en esta tabla",
+    "sInfo":           "Mostrando documentos del _START_ al _END_ de un total de _TOTAL_ documentos",
+    "sInfoEmpty":      "Mostrando documentos del 0 al 0 de un total de 0 documentos",
+    "sInfoFiltered":   "(filtrado de un total de _MAX_ documentos)",
+    "sInfoPostFix":    "",
+    "sSearch":         "Buscar:",
+    "sUrl":            "",
+    "sInfoThousands":  ",",
+    "sLoadingRecords": "Cargando...",
+    "oPaginate": {
+        "sFirst":    "Primero",
+        "sLast":     "Último",
+        "sNext":     "Siguiente",
+        "sPrevious": "Anterior"
+    },
+    "oAria": {
+        "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+        "sSortDescending": ": Activar para ordenar la columna de manera descendente"
     }
+  }
+  });
+  });
+  $(document).ready(function() {
+    $('#dataTableProfile').DataTable( {
+      "language": {
+        "sProcessing":     "Procesando...",
+        "sLengthMenu":     "Mostrar _MENU_ documentos",
+        "sZeroRecords":    "No se encontraron resultados",
+        "sEmptyTable":     "Ningún dato disponible en esta tabla",
+        "sInfo":           "Mostrando documentos del _START_ al _END_ de un total de _TOTAL_ documentos",
+        "sInfoEmpty":      "Mostrando documentos del 0 al 0 de un total de 0 documentos",
+        "sInfoFiltered":   "(filtrado de un total de _MAX_ documentos)",
+        "sInfoPostFix":    "",
+        "sSearch":         "Buscar:",
+        "sUrl":            "",
+        "sInfoThousands":  ",",
+        "sLoadingRecords": "Cargando...",
+        "oPaginate": {
+            "sFirst":    "Primero",
+            "sLast":     "Último",
+            "sNext":     "Siguiente",
+            "sPrevious": "Anterior"
+        },
+        "oAria": {
+            "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+            "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+        }
+       },
+       "order": [[ 3, "desc" ]]
     });
     });
