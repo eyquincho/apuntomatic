ed5a777ad41abda645b560afc470fe5e82b6e87b
Quincho <joaquin@muruais.com>
2020-09-10 17:09:25 +0200

    Añadido Google Analytics

--- footer.php
+++ footer.php
@@ -4,4 +4,13 @@
 			<span>Apuntomatic - <a href="https://github.com/eyquincho/apuntomatic" target="_blank">GitHub</span> 
 		</div>
 	</div>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo $GOOGLE_ANALYTICS_ID; ?>"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', '<?php echo $GOOGLE_ANALYTICS_ID; ?>');
+</script>
 </footer>
