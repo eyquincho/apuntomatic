5bd156089aa3ba1bcce12637fd239aa6bdd7f319
eyquincho <joaquin@muruais.com>
2020-10-02 10:23:06 +0200

    Eliminados restos

--- main.php
+++ main.php
@@ -134,40 +134,6 @@ $head_descargas = mysqli_fetch_assoc($result_head_descargas);
                 </div>
               </div>
             </div>
-
-            <!-- Tarjeta votos positivos, desactivado
-            <div class="col-xl-3 col-md-6 mb-4">
-              <div class="card border-left-info shadow h-100 py-2">
-                <div class="card-body">
-                  <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Votos positivos</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800">##</div>
-                    </div>
-                    <div class="col-auto">
-                      <i class="fas fa-star fa-2x text-gray-300"></i>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div> -->
-
-            <!-- Tarjeta ranking, desactivado
-            <div class="col-xl-3 col-md-6 mb-4">
-              <div class="card border-left-warning shadow h-100 py-2">
-                <div class="card-body">
-                  <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Ranking</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800">##</div>
-                    </div>
-                    <div class="col-auto">
-                      <i class="fas fa-trophy fa-2x text-gray-300"></i>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div> -->
           </div>
 
           <!-- Content Row -->
